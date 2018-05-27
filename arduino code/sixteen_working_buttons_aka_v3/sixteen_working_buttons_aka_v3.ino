18d3341e1cb47adfd7a9482be5cef88ee17e06c2
Echolalien <drakhi.ec@gmail.com>
2018-05-27 21:01:09 +0100

    16 buttons

--- arduino code/sixteen_working_buttons_aka_v3/sixteen_working_buttons_aka_v3.ino
+++ arduino code/sixteen_working_buttons_aka_v3/sixteen_working_buttons_aka_v3.ino
@@ -23,9 +23,12 @@ void setup() {
   for(int i = 3; i<=13; i++){
     pinMode(i, INPUT);
   }
-  for(int i = 0; i<=5; i++){
-    pinMode(A[i], INPUT);
-  }
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
+  pinMode(A2, INPUT);
+  pinMode(A3, INPUT);
+  pinMode(A4, INPUT);
+  pinMode(A5, INPUT);
 }
 
 // the loop routine runs over and over again forever:
@@ -34,9 +37,43 @@ void loop() {
 //  int buttonState1 = digitalRead(pushButton1);
 //  int buttonState2 = digitalRead(pushButton2);
   // print out the state of the button:
-  Serial.print("High: ");
-  Serial.print(buttonState1);
-  Serial.print(", Low: ");
-  Serial.println(buttonState2);
+  //Serial.print("High: ");
+ // Serial.print(buttonState1);
+  //Serial.print(", Low: ");
+  //Serial.println(buttonState2);
+
+  for(int i = 3; i<=13; i++){
+    int state = digitalRead(i);
+    if(state == 1){
+      Serial.print(i);
+      Serial.print(", ");
+    }
+  }
+  int state0 = digitalRead(A0);
+  if(state0==1){
+    Serial.print("A0, ");
+  }
+  int state1 = digitalRead(A1);
+  if(state1==1){
+    Serial.print("A1, ");
+  }
+    int state2 = digitalRead(A2);
+  if(state2==1){
+    Serial.print("A2, ");
+  }
+    int state3 = digitalRead(A3);
+  if(state3==1){
+    Serial.print("A3, ");
+  }
+    int state4 = digitalRead(A4);
+  if(state4==1){
+    Serial.print("A4, ");
+  }
+    int state5 = digitalRead(A5);
+  if(state5==1){
+    Serial.print("A5, ");
+  }
+  Serial.println();
+    
   delay(1);        // delay in between reads for stability
 }
